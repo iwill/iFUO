043653c52166cb660bc3ee8a87c79518cd738aa2
zigzag <zigzag.chen@gmail.com>
2010-01-17 17:19:57 +0800

    Removed hours from tasks

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,10 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100117085530) do
+ActiveRecord::Schema.define(:version => 20100117091901) do
 
   create_table "tasks", :force => true do |t|
     t.string   "name"
-    t.integer  "hours"
     t.boolean  "done"
     t.datetime "created_at"
     t.datetime "updated_at"
